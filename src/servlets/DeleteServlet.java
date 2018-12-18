6e29a9a42776737df0983ae87a41a5650f0983d8
Milos Dragovic <milosh.dr93@yahoo.com>
2018-12-18 01:49:27 +0100

    added project files

---
+++ src/servlets/DeleteServlet.java
@@ -0,0 +1,25 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import domain.EmpDao;
+
+@WebServlet("/DeleteServlet")
+public class DeleteServlet extends HttpServlet {
+	
+	private static final long serialVersionUID = -8586778527176744624L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	
+		String sid = request.getParameter("id");
+		int id = Integer.parseInt(sid);
+		EmpDao.delete(id);
+		response.sendRedirect("ViewServlet");
+	}
+}
